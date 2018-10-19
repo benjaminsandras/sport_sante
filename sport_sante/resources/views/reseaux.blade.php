6fe2c7dece0418a7f963eccde1d4616eb177f185
dylangouzenne <dylan.gouzenne@gmail.com>
2018-10-19 15:02:48 +0200

    ajout route vue et controleur pour reseaux

---
+++ sport_sante/resources/views/reseaux.blade.php
@@ -0,0 +1,11 @@
+@extends('template')
+
+@section('titre')
+Réseaux
+@endsection
+
+@section('contenu')
+
+<h1>Réseaux</h1>
+
+@endsection
--- sport_sante/routes/web.php
+++ sport_sante/routes/web.php
@@ -20,3 +20,4 @@ Route::get('demarche', 'demarcheController@show');
 Route::get('localisation', 'localisationController@show');
 Route::get('structure', 'structureController@show');
 Route::get('activite', 'activiteController@show');
+Route::get('reseaux', 'ReseauxControleur@show');

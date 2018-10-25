fc36ca8bf2249b4eca8997ad1defc4dab6812730
dylangouzenne <dylan.gouzenne@gmail.com>
2018-10-25 17:18:10 +0200

    fonction trie reseaux valider

--- sport_sante/resources/views/reseaux.blade.php
+++ sport_sante/resources/views/reseaux.blade.php
@@ -12,13 +12,14 @@ Réseaux
   <button>test</button>
 </form> -->
 
+<!--
 <div class="list">
   <ul>
-    <!-- Smart select item -->
+    
     <li>
-      <!-- Additional "smart-select" class -->
+      
       <a href="#" class="item-link smart-select smart-select-init">
-        <!-- select -->
+        
         <select name="fruits">
           <option id="1" value="alphabetique" selected>Alphabétique</option>
           <option id="2" value="date_ajout">date d'ajout</option>
@@ -26,14 +27,37 @@ Réseaux
         </select>
         <div class="item-content">
           <div class="item-inner">
-            <!-- Select label -->
+           
             <div class="item-title">Trier par :</div>
           </div>
         </div>
       </a>
     </li>
-    <!-- Another smart selects or list view elements -->
+    
   </ul>
+</div>
+-->
+
+<div>
+  <form method="GET" action="">
+    <label for="choix_trie">Trier par :</label>
+    <button name="choix_trie">
+      <select name="methode_trie">
+        <option value="1" >Alphabétique</option>
+        <option value="2" selected>Date d'ajout</option>
+        <!-- 
+        option a rajouter plus tard
+
+        <option value="3" >Localisation</option>
+        -->
+      </select>
+    </button>
+    <br>
+    <br>
+    <input type="submit" name="submit">
+    <br>
+    <br>
+  </form>
 </div>
 
 <table>
